c78a96b9a3558ccf79d896c56cb5ba73f6a2963d
Annie Ke <annieke8@gmail.com>
2021-11-09 12:38:45 -0800

    wip: surgery predeploy tests

--- packages/regenesis-surgery/test/predeploy.spec.ts
+++ packages/regenesis-surgery/test/predeploy.spec.ts
@@ -1,56 +1,178 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
 import { expect, env } from './setup'
-import { AccountType, Account } from '../scripts/types'
+import { AccountType } from '../scripts/types'
+import { GenesisJsonProvider } from './provider'
 
 describe('predeploys', () => {
-  let predeploys = {
+  const predeploys = {
     eth: [],
     newNotEth: [],
     noWipe: [],
     wipe: [],
     weth: [],
   }
+  // Base genesis file only
+  let genesisStateProvider: GenesisJsonProvider
+  // Old sequencer state
+  let oldStateProvider: GenesisJsonProvider
 
   before(async () => {
     await env.init()
     predeploys.eth = env.getAccountsByType(AccountType.PREDEPLOY_ETH)
-    predeploys.newNotEth = env.getAccountsByType(AccountType.PREDEPLOY_NEW_NOT_ETH)
+    predeploys.newNotEth = env.getAccountsByType(
+      AccountType.PREDEPLOY_NEW_NOT_ETH
+    )
     predeploys.noWipe = env.getAccountsByType(AccountType.PREDEPLOY_NO_WIPE)
     predeploys.wipe = env.getAccountsByType(AccountType.PREDEPLOY_WIPE)
     predeploys.weth = env.getAccountsByType(AccountType.PREDEPLOY_WETH)
+
+    genesisStateProvider = new GenesisJsonProvider(
+      env.surgeryDataSources.genesis
+    )
+    oldStateProvider = new GenesisJsonProvider(
+      env.surgeryDataSources.configs.stateDumpFilePath
+    )
   })
 
-  it('predeploy tests', () => {
-    describe.skip('new predeploys that are not ETH', () => {
-      it('should have the exact state specified in the base genesis file', async () => {})
-    })
+  describe('new predeploys that are not ETH', () => {
+    for (const [i, account] of predeploys.newNotEth.entries()) {
+      describe(`account ${i}/${predeploys.newNotEth.length} (${account.address})`, () => {
+        it('should have the exact state specified in the base genesis file', async () => {
+          const preBytecode = await genesisStateProvider.getCode(
+            account.address
+          )
+          const postBytecode = await env.postL2Provider.getCode(account.address)
+          expect(preBytecode).to.eq(postBytecode)
 
-    describe.skip('predeploys where the old state should be wiped', () => {
-      it('should have the code and storage of the base genesis file', async () => {})
+          // check storage
 
-      it('should have the same nonce and balance as before', async () => {})
-    })
+          const preNonce = await genesisStateProvider.getTransactionCount(
+            account.address,
+            env.config.stateDumpHeight
+          )
+          const postNonce = await env.postL2Provider.getTransactionCount(
+            account.address
+          )
+          expect(preNonce).to.deep.eq(postNonce)
 
-    describe.skip('predeploys where the old state should be preserved', () => {
-      it('should have the code of the base genesis file', async () => {})
+          const preBalance = await genesisStateProvider.getBalance(
+            account.address,
+            env.config.stateDumpHeight
+          )
+          const postBalance = await env.postL2Provider.getBalance(
+            account.address
+          )
+          expect(preBalance).to.deep.eq(postBalance)
+        })
+      })
+    }
+  })
 
-      it('should have the combined storage of the old and new state', async () => {})
+  describe('predeploys where the old state should be wiped', () => {
+    for (const [i, account] of predeploys.wipe.entries()) {
+      describe(`account ${i}/${predeploys.wipe.length} (${account.address})`, () => {
+        it('should have the code and storage of the base genesis file', async () => {
+          const preBytecode = await genesisStateProvider.getCode(
+            account.address
+          )
+          const postBytecode = await env.postL2Provider.getCode(account.address)
+          expect(preBytecode).to.eq(postBytecode)
 
-      it('should have the same nonce and balance as before', async () => {})
-    })
+          // check storage
+        })
 
-    describe.skip('OVM_ETH', () => {
-      it('should have disabled ERC20 features', async () => {})
+        it('should have the same nonce and balance as before', async () => {
+          const preNonce = await oldStateProvider.getTransactionCount(
+            account.address,
+            env.config.stateDumpHeight
+          )
+          const postNonce = await env.postL2Provider.getTransactionCount(
+            account.address
+          )
+          expect(preNonce).to.deep.eq(postNonce)
 
-      it('should no recorded balance for the contracts that move to WETH9', async () => {})
+          const preBalance = await oldStateProvider.getBalance(
+            account.address,
+            env.config.stateDumpHeight
+          )
+          const postBalance = await env.postL2Provider.getBalance(
+            account.address
+          )
+          expect(preBalance).to.deep.eq(postBalance)
+        })
+      })
+    }
+  })
 
-      it('should have a new balance for WETH9 equal to the sum of the moved contract balances', async () => {})
-    })
+  describe('predeploys where the old state should be preserved', () => {
+    for (const [i, account] of predeploys.noWipe.entries()) {
+      describe(`account ${i}/${predeploys.noWipe.length} (${account.address})`, () => {
+        it('should have the code of the base genesis file', async () => {
+          const preBytecode = await genesisStateProvider.getCode(
+            account.address
+          )
+          const postBytecode = await env.postL2Provider.getCode(account.address)
+          expect(preBytecode).to.eq(postBytecode)
+        })
 
-    describe.skip('WETH9', () => {
-      it('should have balances for each contract that should move', async () => {})
+        it('should have the combined storage of the old and new state', async () => {
+          // check storage
+        })
 
-      it('should have a balance equal to the sum of all moved balances', async () => {})
-    })
+        it('should have the same nonce and balance as before', async () => {
+          const preNonce = await oldStateProvider.getTransactionCount(
+            account.address,
+            env.config.stateDumpHeight
+          )
+          const postNonce = await env.postL2Provider.getTransactionCount(
+            account.address
+          )
+          expect(preNonce).to.deep.eq(postNonce)
+
+          const preBalance = await oldStateProvider.getBalance(
+            account.address,
+            env.config.stateDumpHeight
+          )
+          const postBalance = await env.postL2Provider.getBalance(
+            account.address
+          )
+          expect(preBalance).to.deep.eq(postBalance)
+        })
+      })
+    }
+  })
+
+  describe('OVM_ETH', () => {
+    for (const [i, account] of predeploys.eth.entries()) {
+      describe(`account ${i}/${predeploys.eth.length} (${account.address})`, () => {
+        it('should have disabled ERC20 features', async () => {
+          // .transfer should not work? need live provider
+        })
+
+        it('should have a new balance for WETH9 equal to the sum of the moved contract balances', async () => {
+          // need live provider for WETH balances
+        })
+      })
+    }
+  })
+
+  describe('WETH9', () => {
+    for (const [i, account] of predeploys.weth.entries()) {
+      describe(`account ${i}/${predeploys.weth.length} (${account.address})`, () => {
+        it('should no recorded ETH balance', async () => {
+          const postBalance = await env.postL2Provider.getBalance(
+            account.address
+          )
+          expect(postBalance.toNumber()).to.eq(0)
+        })
+
+        it('should have WETH balances for each contract that should move', async () => {
+          // need live provider for WETH balances
+        })
+
+        it('should have a balance equal to the sum of all moved balances', async () => {
+          // need live provider for WETH balances
+        })
+      })
+    }
   })
 })
